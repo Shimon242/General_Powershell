7147622895b3d76c92d20c8baf3fc839f516bf95
Shimon Brathwaite <46653837+Shimon242@users.noreply.github.com>
2020-08-17 18:54:55 -0400

    Update Incident_Response.ps1
    Common powershell commands used for incident response, see the source article here: https://www.securityinbits.com/incident-response/powershell-commands-for-incident-response/

--- Incident_Response.ps1
+++ Incident_Response.ps1
@@ -14,10 +14,10 @@ Get-ItemProperty 'Item Path' | Remove-ItemProperty -Name 'Service Name'
 
 # To get file hashes
 # SHA 256 (This is the default of the get-FileHash cmdlet)
-Get-FileHash -Path 'C:\Users\admin\AppData\Roaming\0319B08F-2B65-4192-B2D2-1E2F62087064\IMAP Service\imapsv.exe'
+Get-FileHash -Path 'C:[Path]'
 
 # MD5
-Get-FileHash -Algorithm MD5 -Path 'C:\Users\admin\AppData\Roaming\0319B08F-2B65-4192-B2D2-1E2F62087064\IMAP Service\imapsv.exe'
+Get-FileHash -Algorithm MD5 -Path 'C:[Path]'
 
 # Create New Item
 
@@ -25,10 +25,10 @@ New-Item -ItemType Directory -Path C:\Users\admin\IoCs
 
 # Copy Item
 
-Copy-Item C:\Users\admin\AppData\Roaming\0319B08F-2B65-4192-B2D2-1E2F62087064\ -Destination C:\Users\admin\IoCs\ -Recurse
+Copy-Item C:\[Source Path] -Destination C:[Destination Path] -Recurse
 
 # Delete an Item
 
-Remove-Item -Path $env:APPDATA\0319B08F-2B65-4192-B2D2-1E2F62087064\ -Recurse -Force
+Remove-Item -Path [Item Path] -Recurse -Force
 
-Remove-Item -Path $env:USERPROFILE\Desktop\Serial.exe
+Remove-Item -Path [Item Path]
